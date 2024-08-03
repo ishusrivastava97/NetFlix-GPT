c8043a6bee40d3ec4154ec362511ebda17128a88
ishusrivastava97 <ishusrivastava4703@gmail.com>
2024-08-03 21:16:46 +0530

    removed comments and console logs

--- src/hooks/useMoviesTrailer.js
+++ src/hooks/useMoviesTrailer.js
@@ -1,26 +1,29 @@
-import { useDispatch } from "react-redux";
-import { API_OPTIONS } from "../utils/Constant";
 import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { API_OPTIONS } from "../utils/Constant";
 import { addTrailerVideo } from "../utils/moviesSlice";
 
-const useMoviesTrailer =({movieId})=>{
-      const dispatch = useDispatch();
-      const getMoviesVideos = async () => {
-        const data = await fetch(
-          "https://api.themoviedb.org/3/movie/movie_id/videos?language=en-US",API_OPTIONS
-        );
-        
-        const json = data.json();
-        console.log(json);
-         console.log(json.results)
-        const filterData = json.results.filter(
-          (video) => video.type === "Trailer"
-        );
-        const trailer = filterData.length ? filterData[0] : json.results[0];
-        dispatch(addTrailerVideo(trailer));
-      };
-      useEffect(() => {
-        getMoviesVideos();
-      }, [] );
-}
-export default useMoviesTrailer;
+const useMovieTrailer = (movieId) => {
+  const dispatch = useDispatch();
+
+  const trailerVideo = useSelector((store) => store.movies.trailerVideo);
+
+  const getMovieVideos = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/" +
+        movieId +
+        "/videos?language=en-US",
+      API_OPTIONS
+    );
+    const json = await data.json();
+
+    const filterData = json.results.filter((video) => video.type === "Trailer");
+    const trailer = filterData.length ? filterData[0] : json.results[0];
+    dispatch(addTrailerVideo(trailer));
+  };
+  useEffect(() => {
+    !trailerVideo && getMovieVideos();
+  }, []);
+};
+
+export default useMovieTrailer;
