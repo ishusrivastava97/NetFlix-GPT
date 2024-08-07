d90e00e784622535412e78d34c3fdde3d5af22f1
ishusrivastava97 <ishusrivastava4703@gmail.com>
2024-08-08 00:54:30 +0530

    made secondary container with moviesList

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -4,10 +4,10 @@ const VideoTitle = ({title ,overview}) => {
       <h1 className="text-6xl font-bold">{title}</h1>
       <p className="py-6 text-lg w-1/4">{overview}</p>
       <div>
-        <button className="bg-white text-black p-4 px-12 text-xl w-10  rounded-lg mx-2 hover:bg-opacity-80">
-          Play
+        <button className=" bg-white text-black py-1 md:py-4 px-3 md:px-12 text-xl  rounded-lg hover:bg-opacity-80">
+          ▶️ Play
         </button>
-        <button className="bg-black text-white p-4 px-12 text-xl w-10 bg-opacity-10 rounded-lg hover:bg-opacity-80">
+        <button className="hidden md:inline-block mx-2  bg-gray-500 text-white p-4 px-12 text-xl bg-opacity-50 rounded-lg">
           More Info
         </button>
       </div>
