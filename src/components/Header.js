52122d827f07bfe6ee441ad17fae8ce0d2c90c95
ishusrivastava97 <ishusrivastava4703@gmail.com>
2024-08-08 17:47:36 +0530

    Built Multi Language Page

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,15 @@ import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { addUser, removeUser } from "../utils/userSlice";
-import { LOGO } from "../utils/Constant";
+import { LOGO, SUPPORTED_LANGUAGES } from "../utils/Constant";
 import { toggleGptSearchView } from "../utils/gptSlice";
+import { changeLanguage } from "../utils/configSlice";
 
 const Header = () => {
   const dispatch=useDispatch()
   const Navigate = useNavigate();
   const user = useSelector((store) => store.user);
+  const showGptSearch = useSelector((store) => store.gpt.showGptSearch);
   const handleSignOut = () => {
     signOut(auth)
       .then(() => {
@@ -46,14 +48,32 @@ const Header = () => {
   const handleGPTSearchClick=()=>{
     dispatch(toggleGptSearchView());
   }
+  const handleLanguageChange=(e)=>{
+    dispatch(changeLanguage(e.target.value));
+  };
   return (
     <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 justify-between flex ">
       <img className="w-44" src={LOGO} alt="logo Here" />
       {user && (
         <div className="flex p-2">
           {/* const photoURL */}
-          <button className="py-2 px-4 mx-4 my-2 bg-purple-800 text-white rounded-lg" onClick={handleGPTSearchClick}>
-            GPT Search
+          {showGptSearch && (
+            <select
+              className="py-2 px-4 mx-4 my-2 rounded-lg bg-gray-900 text-white"
+              onChange={handleLanguageChange}
+            >
+              {SUPPORTED_LANGUAGES.map((lang) => (
+                <option key={lang.identifier} value={lang.identifier}>
+                  {lang.name}
+                </option>
+              ))}
+            </select>
+          )}
+          <button
+            className="py-2 px-4 mx-4 my-2 bg-purple-800 text-white rounded-lg"
+            onClick={handleGPTSearchClick}
+          >
+            {showGptSearch ? "Home Page" : "GPT Search"}
           </button>
           <img className="w-12 h-12" alt="user icon" src={user.photoURL} />
 
